e880a2bc8cf3fdac9b48abcc1c47db5c2ba72495
lidertic <lidertic@gmail.com>
2019-05-21 15:11:07 +0200

    filtre a les subscripcions

--- src/app/_shared/nav/nav.component.ts
+++ src/app/_shared/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/auth.service';
 import { Missatge } from 'src/app/_services/model/missatges';
 import { MessageService } from 'src/app/_services';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav',
@@ -15,14 +16,16 @@ export class NavComponent implements OnInit {
   constructor(private sessio: AuthService, private data: MessageService) {
     this._sesio = this.sessio.isLoggedIn;
 
-    this.data.currentMessage.subscribe(message => {
-      this.message = message;
-      if (message.tipus === 'usuari' && message.getObjectAtribut('actiu')) {
-        this._sesio = true;
-      } else {
-        this._sesio = false;
-      }
-    });
+    this.data.currentMessage
+      .pipe(filter(data => data.tipus === 'usuari'))
+      .subscribe(message => {
+        this.message = message;
+        if (message.getObjectAtribut('actiu')) {
+          this._sesio = true;
+        } else {
+          this._sesio = false;
+        }
+      });
   }
 
   ngOnInit() {}
